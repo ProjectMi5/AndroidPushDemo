b1d0e45a0800b935d9ebad4baa946092005dd765
Oliver Wangler <oliver@wngr.de>
2015-05-04 13:44:25 +0200

    Initial commit

---
+++ app/src/main/java/mi5/pushtest/ApplicationConstants.java
@@ -0,0 +1,11 @@
+package mi5.pushtest;
+
+/**
+ * Created by ow on 04.05.15.
+ */
+public class ApplicationConstants {
+
+    static final String APP_SERVER_URL = "https://wngr.de/gcm/gcm.php?shareRegId=true";
+    static final String GOOGLE_PROJ_ID = "519296367272";
+    static final String MSG_KEY = "m";
+}
